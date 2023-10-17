c38d5cd822c75990047f4070027d56f15353f704
Laura <laurapicon98@gmail.com>
2023-10-17 21:22:03 +0200

    Introducción a standalone components

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+// import { CommonModule } from '@angular/common';
+
+
+@Component({
+  standalone: true,
+  imports: [CounterAloneComponent],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.css']
+})
+export class AlonePageComponent {
+
+  constructor() {}
+
+}
