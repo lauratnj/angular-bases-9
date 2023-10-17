4de6bbea662b7a0e8c92a44289d95c7443a3420d
Laura <laurapicon98@gmail.com>
2023-10-17 20:27:07 +0200

    Lista de casas y componente mini mapa

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
+interface PlainMarker {
+  color: string,
+  lngLat: number[]
+}
+
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-3.709227831924494, 40.47694217873362);
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap?.nativeElement ) throw "Map div not found";
+    if ( !this.lngLat ) throw "LngLat can't be null";
+
+    // mapa
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+    // marker
+    new Marker()
+      .setLngLat( this.lngLat )
+      .addTo( this.map )
+
+
+  }
 
 }
