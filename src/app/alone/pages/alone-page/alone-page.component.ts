d2cd7e047aa8adfff55f49efaef765446bb1eb09
Laura <laurapicon98@gmail.com>
2023-10-18 19:26:18 +0200

    Tranformar un componente normal a standalone y arreglar problemas

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 // import { CommonModule } from '@angular/common';
 
 
 @Component({
   standalone: true,
-  imports: [CounterAloneComponent],
+  imports: [CounterAloneComponent, SideMenuComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -7,20 +7,19 @@ import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-g
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 
 @NgModule({
   declarations: [
     MapsLayoutComponent,
-    SideMenuComponent,
     MiniMapComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -31,6 +30,7 @@ import { CounterAloneComponent } from '../alone/components/counter-alone/counter
     CommonModule,
     MapsRoutingModule,
     CounterAloneComponent,
+    SideMenuComponent,
   ]
 })
 export class MapsModule { }
